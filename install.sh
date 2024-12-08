697918f0373ad56ef8b0a0734e5129708a4c7fc0
hydraPV <crescente.a.cm@gmail.com>
2024-12-08 17:18:35 -0300

    8/12/2024

--- install.sh
+++ install.sh
@@ -1,24 +1,17 @@
 # If there is a config directory: delete it and make another, if not, go to next command
-if [ -d ~/Downloads/config ]; then
-	rm -r -f ~/Downloads/.config
-	mkdir ~/Downloads/.config
+if [ -d ~/.config ]; then
+	rm -r -f ~/.config
+	mkdir ~/.config
 	echo "Succesfully deleted and created config directory"
 else
 	echo "Succesfully created config directory"
 fi
 
 # Move config folder to the correct location
-mv /home/hydra/hydra-dotfiles/config* ~/Downloads/.config
-
-if [ -d ~/Downloads/.config/ ]; then 
-echo "Done"
-else
-echo "Something went wrong"
-fi
-
+mv /home/hydra/hydra-dotfiles/config* ~/.config
 
 # Checks if all went right
-if [ -d ~/Downloads/.config/ ]; then
+if [ -d ~/.config/ ]; then
 echo "Config should be applied, press SUPER + ALT + R to apply"
 else
 echo "Something went wrong on the config install"
