24ff4fe89a4c495772654daf3e7a762268115058
hydraPV <crescente.a.cm@gmail.com>
2024-12-07 22:41:45 -0300

    7/12/2024

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+function fish_prompt
+
+  # interactive user name @ host name, date/time in YYYY-mm-dd format and path
+
+  set_color 81A1C1; echo -n  [(prompt_pwd)] 
+  set_color d8dee9; echo -n  Hydra
+  set_color 8fbcbb; echo -n " 󰣇  >"
+
+end
